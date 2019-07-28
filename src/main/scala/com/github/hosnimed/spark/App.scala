8d81bb965f3f14ad71ba0a97eef4980da60c3cac
Mohamed ELHOSNI <medhosni@gmail.com>
2019-07-28 18:40:56 +0200

    Verify sparkSession, input and output

--- src/main/scala/com/github/hosnimed/spark/App.scala
+++ src/main/scala/com/github/hosnimed/spark/App.scala
@@ -1,12 +1,17 @@
 package com.github.hosnimed.spark
 
-import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions._
+import junit.framework.Assert
 
-object App {
-
-  def happyData()(df: DataFrame): DataFrame = {
-    df.withColumn("happy", lit("data is fun"))
-  }
+object App extends App with SparkSessionWrapper {
+  Assert.assertNotNull(spark)
+  val description =
+    """
+      |Program arguments :
+      |Input CSV (Default : src/main/resources/xag.csv )
+      |Output Directory (Default : src/main/resources/)
+      |""".stripMargin
+  println(description)
+  val input_csv = if(args.isEmpty) "src/main/resources/xag.csv" else args(0)
+  val path = if(args.isEmpty) "src/main/resources/" else args(1)
 
 }
